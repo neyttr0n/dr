5820df4018fd97a05ef90e12d0477bc201b5835e
neyttr0n <ceratin1999@yandex.ru>
2022-12-15 15:18:41 +0300

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Map.Entry; //ипрро
 
 public class Main {
 
